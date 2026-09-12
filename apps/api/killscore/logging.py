4cfbb840d00220b85ef5982ca5729ced189aad45
kirito2056 <gwonhajin2@gmail.com>
2026-09-12 04:12:09 +0000

    ADD :: structlog 기반 JSON 구조화 로그

---
+++ apps/api/killscore/logging.py
@@ -0,0 +1,27 @@
+import logging
+import sys
+
+import structlog
+
+from killscore.settings import settings
+
+
+def configure_logging() -> None:
+    renderer = structlog.processors.JSONRenderer() if settings.log_json else structlog.dev.ConsoleRenderer()
+    structlog.configure(
+        processors=[
+            structlog.contextvars.merge_contextvars,
+            structlog.processors.add_log_level,
+            structlog.processors.TimeStamper(fmt="iso", utc=True),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            renderer,
+        ],
+        wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
+        logger_factory=structlog.PrintLoggerFactory(sys.stdout),
+        cache_logger_on_first_use=True,
+    )
+
+
+def get_logger(name: str):
+    return structlog.get_logger(name)
